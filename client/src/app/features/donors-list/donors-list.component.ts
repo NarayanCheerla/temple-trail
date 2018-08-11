1c84d63b00ff31d8df125ebfe9783f8034afe586
Bineesh B <bineesh2107@gmail.com>
2018-08-11 16:31:35 +0530

    Added other pages and updated app navigation

---
+++ client/src/app/features/donors-list/donors-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donors-list',
+  templateUrl: './donors-list.component.html',
+  styleUrls: ['./donors-list.component.scss']
+})
+export class DonorsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- client/src/app/shared/app-header/app-header.component.ts
+++ client/src/app/shared/app-header/app-header.component.ts
@@ -7,20 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppHeaderComponent implements OnInit {
   links = [
-    {
-      label: 'Login',
-      route: '/login'
-    },
-    {
-      label: 'Dashboard',
-      route: '/dashboard'
-    }
+    { label: 'Login', route: '/login', icon: '' },
+    { label: 'Dashboard', route: '/dashboard', icon: 'home' },
+    { label: 'Add Details', route: '/addDetails', icon: 'add_circle_outline' },
+    { label: 'Search', route: '/search', icon: 'search' },
+    { label: 'Donors', route: '/donors', icon: 'group' }
   ];
+
   activeLink = this.links[0].label;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
